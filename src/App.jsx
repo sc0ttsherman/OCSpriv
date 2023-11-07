a4bebbbd760b2fd433823b0225ab9ec123be9db3
Scott M. Sherman <scottsherman@gmail.com>
2023-11-06 21:06:14 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import ocsLogo from './assets/noaa-logo-rgb-2022.png'
+import Image from './Image'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+        <a href="https://react.dev" target="_blank">
+          <img src={ocsLogo} className="logo" alt="React logo" />
+        </a>
+      </div>
+      <h1>Andrew &amp; Dad</h1>
+      <div><Image></Image></div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+       
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Image.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Image() {
+
+    let imageNum = 1;
+
+
+    return <h1>hello world</h1>;
+}
+
+export default Image
